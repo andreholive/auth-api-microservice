8ce263b03b6b29de12e30628f9b5274396a8df5e
andreolive <andreolive@live.com>
2023-02-24 15:31:55 -0300

    redefinindo estrutura das pastas

--- auth-api-backend/src/database/index.ts
+++ auth-api-backend/src/database/index.ts
@@ -2,7 +2,7 @@ import { DataSource } from "typeorm";
 
 export const AppDataSource = new DataSource({
     type: "mysql",
-    host: "localhost",
+    host: "db",
     port: 3306,
     username: "root",
     password: "senha123",
